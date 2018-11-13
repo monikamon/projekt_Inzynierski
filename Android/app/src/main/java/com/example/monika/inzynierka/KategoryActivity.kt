ab4e1de027e296c7aab744c42c15def6e9ffd97a
Monika <monika.struk72@gmail.com>
2018-11-13 21:45:19 +0100

    Pierwsze ekrany

---
+++ Android/app/src/main/java/com/example/monika/inzynierka/KategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.monika.inzynierka
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class KategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kategory)
+    }
+}
